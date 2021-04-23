93e08d8bf3d9441195b31c9e11cee306e47e8f60
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-04-23 10:55:52 -0700

    developed initial comments section with view form, controller, and db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_23_054146) do
+ActiveRecord::Schema.define(version: 2021_04_23_172626) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -37,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_04_23_054146) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "comments", "posts"
 end
