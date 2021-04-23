5bcf0dcd13f921ce891a013db70deacba3670584
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-04-23 13:21:46 -0700

    updated comments function, will now render appropriately

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
 
          validates :username, presence: true
          has_many :posts
+        #  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_23_172626) do
+ActiveRecord::Schema.define(version: 2021_04_23_200801) do
 
   create_table "comments", force: :cascade do |t|
+    # t.string "name"
     t.text "body"
     t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
