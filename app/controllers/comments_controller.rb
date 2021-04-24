5980db3a41599ab541e8318f93eba1304455af43
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-04-23 18:55:26 -0700

    developed function to edit user comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+    before_action :find_post, only: [:create, :edit, :update, :destroy]
     before_action :authenticate_user!
 
     def create
@@ -13,5 +14,30 @@ class CommentsController < ApplicationController
         @comment = @post.comments.find(params[:id])
         @comment.destroy
         redirect_to post_path(@post)
+    end
+
+    def edit
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+    end
+    
+    def update
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+
+        if @comment.update(comment_params)
+            redirect_to post_path(@post)
+        else
+            render 'edit'
+        end
+    end
+
+    private
+    def find_post
+        @post = Post.find(params[:post_id])
+    end
+
+    def comment_params
+        params.require(:comment).permit(:name, :body, :user_id, :username)
     end
 end
