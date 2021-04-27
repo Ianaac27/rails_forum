d6e838c3d23e848dc262a475181a225b3d7f4e05
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-04-27 10:29:49 -0700

    developed function for user to add picture to post with Active Storage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -78,6 +78,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :content, :user_id, :username)
+      params.require(:post).permit(:title, :content, :image, :user_id, :username)
     end
 end
